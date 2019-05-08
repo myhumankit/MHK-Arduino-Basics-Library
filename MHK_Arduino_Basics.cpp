528e55a10a336b1112785eb8c358646c016d165d
Julien Lebunetel <julien@lebunetel.com>
2019-05-08 10:42:33 +0200

    First commit

---
+++ MHK_Arduino_Basics.cpp
@@ -0,0 +1,28 @@
+// MHK_Arduino_Basics
+// Usefull tools to start prototyping with Arduino.
+// Copyright MyHumanKit 2019
+// MIT License
+
+// include Arduino core library
+#include "Arduino.h"
+
+// include this library's description file
+#include "MHK_Arduino_Basics.h"
+
+// include description files for other libraries used
+// ...
+
+// Constructor /////////////////////////////////////////////////////////////////
+// Function that handles the creation and setup of instances
+
+// ...
+
+// Public Methods //////////////////////////////////////////////////////////////
+// Functions available in MHK_Arduino_Basics sketches, this library, and other libraries
+
+// ...
+
+// Private Methods /////////////////////////////////////////////////////////////
+// Functions only available to other functions in this library
+
+// ...
---
+++ MHK_Arduino_Basics.h
@@ -0,0 +1,19 @@
+// MHK_Arduino_Basics
+// Usefull tools to start prototyping with Arduino.
+// Copyright MyHumanKit 2019
+// MIT License
+
+// ensure this library description is only included once
+#ifndef MHK_Arduino_Basics
+#define MHK_Arduino_Basics
+
+// uncomment to activate debugging mode
+//#define DEBUG 1
+
+// include description files for other libraries used
+// ...
+
+// library interface description
+// ...
+
+#endif
